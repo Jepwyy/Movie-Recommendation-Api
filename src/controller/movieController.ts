0ba859539abd85f54197d4571554bb98106b10fb
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-10-01 21:39:17 +0800

    add movie

--- src/controller/movieController.ts
+++ src/controller/movieController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
-import { findAllMovies } from '../services/movieService'
+import { createMovie, findAllMovies } from '../services/movieService'
 
-export async function getMovies(req: Request, res: Response) {
+async function getMovies(req: Request, res: Response) {
   try {
     const movies = await findAllMovies()
     res.status(200).json(movies)
@@ -10,3 +10,22 @@ export async function getMovies(req: Request, res: Response) {
     res.status(500).json({ error: 'Internal server error' })
   }
 }
+async function addMovies(req: Request, res: Response) {
+  try {
+    const { title, genres } = req.body
+    if (!title || !genres) {
+      return res
+        .status(400)
+        .json({ message: 'Please Enter All Required Fields!' })
+    }
+    const movie = await createMovie({
+      title,
+      genres,
+    })
+    res.status(200).json({ message: 'Movie added successfully', movie })
+  } catch (error) {
+    console.error('Error fetching movies:', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
+export default { getMovies, addMovies }
--- src/routes/movieRouter.ts
+++ src/routes/movieRouter.ts
@@ -1,7 +1,8 @@
 import express from 'express'
-import { getMovies } from '../controller/movieController'
+import Movies from '../controller/movieController'
 const movieRouter = express.Router()
 
-movieRouter.get('/all', getMovies)
+movieRouter.get('/all', Movies.getMovies)
+movieRouter.get('/create', Movies.addMovies)
 
 export default movieRouter
